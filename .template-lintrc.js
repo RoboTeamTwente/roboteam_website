17842b094e35bf7d3642d59ec89b3737394348ed
mrlukasbos <mrlukasbos@gmail.com>
2018-09-30 11:50:05 +0200

    Fix all linter issues

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,5 +1,11 @@
 'use strict';
 
 module.exports = {
-  extends: 'recommended'
+  extends: 'recommended',
+
+   rules: {
+    'attribute-indentation': false,
+    'no-invalid-interactive': false,
+    'quotes': false
+  }
 };
