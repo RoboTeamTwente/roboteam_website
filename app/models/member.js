f3cd0ec38d38be656f8cfeb3786f4913aa6fc9db
mrlukasbos <mrlukasbos@gmail.com>
2018-09-30 15:56:25 +0200

    Revert "Add mail addresses for each member"
    This reverts commit 3496cdbfdbd8c261fb0da0509e122b5b2e1082c8.

--- app/models/member.js
+++ app/models/member.js
@@ -9,7 +9,6 @@ export default Model.extend({
   imageSrc: attr('string'),
   role: attr('string'),
   study: attr('string'),
-  mail: attr('string'),
   story: attr('string'),
 
   storyFormatted: computed('story', function(){
