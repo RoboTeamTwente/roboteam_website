6a3776c7781c70fb4659e894633e47574e6c0919
RobZelluf <robzelluf@hotmail.com>
2018-11-09 14:22:22 +0100

    Fix indentations

--- app/components/instagram-feed.js
+++ app/components/instagram-feed.js
@@ -9,16 +9,7 @@ export default Component.extend({
         accessToken: '5417282768.1e64ccd.d45ff90e542745f79dab32601bc7ce3d',
         template: '<a class="col-6 col-md-3 mb-3" href="{{link}}"><img class="insta-image" src="{{image}}"/></a>',
         limit: 16,
-        resolution: 'low_resolution',
-        // success: function(response){
-        //   response.data.forEach(function(e){
-        //     e.images.thumbnail = {
-        //       url: e.images.thumbnail.url.replace('150x150', '640x640'),
-        //       width: 640,
-        //       height: 640
-        //     };
-        //   });
-        // }
+        resolution: 'low_resolution'
     });
     feed.run();
   },
