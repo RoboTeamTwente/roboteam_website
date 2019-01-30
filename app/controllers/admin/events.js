f1a0d245a69850160d1e5433d8291e95a1394929
mrlukasbos <mrlukasbos@gmail.com>
2019-01-30 16:06:54 +0100

    Continue working on events

---
+++ app/controllers/admin/events.js
@@ -0,0 +1,34 @@
+import Controller from '@ember/controller';
+import computed from '@ember/object'
+
+export default Controller.extend({
+  title: null,
+  location: null,
+  startDate: new Date(),
+  endDate: new Date(),
+  actions: {
+    addEvent: function() {
+	    let title = this.get('title').trim(); // trim to reduce whitespaces
+	    let location = this.get('location').trim();
+
+	    // if there is a title it is okay-ish
+	    if (title != "" && title != null) {
+		    var newEvent = this.store.createRecord('event', {
+		      title: title,
+		      location: location,
+		      startDate: this.get('startDate'),
+		      endDate: this.get('endDate')
+		    });
+	   		newEvent.save();
+	   	}
+    },
+    removeEvent: function(id) {
+    	this.store.findRecord('event', id).then(function(cf) {
+    		cf.destroyRecord();
+    	});
+    }
+}
+});
+
+
+  
