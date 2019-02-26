6a0c6034d101bab91cbaa75ad0623e5c16f98461
mrlukasbos <mrlukasbos@gmail.com>
2019-02-26 11:45:21 +0100

    Also add pagination to events

--- app/controllers/admin/events/index.js
+++ app/controllers/admin/events/index.js
@@ -1,8 +1,10 @@
 import Controller from '@ember/controller';
 import removeModelAction from 'roboteam-website/mixins/remove-model-action';
+import paginationMixin from 'roboteam-website/mixins/pagination-mixin'
 
-export default Controller.extend(removeModelAction, {
+export default Controller.extend(removeModelAction, paginationMixin, {
   modelType: "event",
-  modelNameProperty: "title"
+  modelNameProperty: "title",
+  page: 1,
+  perPage: 10
 });
-
--- app/controllers/events/index.js
+++ app/controllers/events/index.js
@@ -1,10 +1,7 @@
 import Controller from '@ember/controller';
-import { computed } from '@ember/object';
 import paginationMixin from 'roboteam-website/mixins/pagination-mixin'
 
 export default Controller.extend(paginationMixin, {
   page: 1,
-  perPage: 6,
+  perPage: 6
 });
-
- 
