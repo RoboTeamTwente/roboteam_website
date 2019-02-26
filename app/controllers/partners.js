05b7f906cbfb15c816ccbdaf9c22f8574e736574
mrlukasbos <mrlukasbos@gmail.com>
2019-02-26 17:57:55 +0100

    apply settings as service

--- app/controllers/partners.js
+++ app/controllers/partners.js
@@ -1,7 +1,9 @@
 import Controller from '@ember/controller';
 import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default Controller.extend({
+	settings: service(),
 	gigaByteSponsors: computed('model.sponsors', function() {
 		return this.get('model.sponsors').filter(function(sponsor) {
 			return sponsor.get('package') === "GIGABYTE";
--- app/models/setting.js
+++ app/models/setting.js
@@ -29,6 +29,19 @@ export default Model.extend({
   	return null;
   }),
 
+  valueShortened: computed('value', function() {
+    if (this.get('type') === 'string') {
+      let content = this.get('value');
+      const maxLength = 100; // max amount of characters
+
+      // cut off to the last space before maxlength
+      if (content.length <= maxLength) return content;
+      return content.substr(0, content.lastIndexOf(" ", maxLength)) + "...";
+    } else {
+      return this.get('value');
+    }
+  }),
+
   createdAt: attr('date'),
   updatedAt: attr('date')
 });
