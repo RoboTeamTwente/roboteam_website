d22af4f5987bbfbf6a258a0feb73a1613a7f2240
mrlukasbos <mrlukasbos@gmail.com>
2019-10-09 12:08:43 +0200

    proper descension sort for previousteams

--- app/routes/previousteams.js
+++ app/routes/previousteams.js
@@ -2,6 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model() {    
-    return this.store.findAll('previousteam').then(previousteams => previousteams.sortBy('yearOfStart DESC'));
+    return this.store.findAll('previousteam').then(previousteams => previousteams.sortBy('yearOfStart').reverse());
   }
 });
