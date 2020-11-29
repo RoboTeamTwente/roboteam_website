a709bf6e3d05d06482ee76d701d3169b05a017a9
Lukas Bos <mrlukasbos@gmail.com>
2020-11-29 16:33:23 +0100

    fix auth

--- app/torii-adapters/application.js
+++ app/torii-adapters/application.js
@@ -1,3 +1,15 @@
-import ToriiFirebaseAdapter from 'emberfire/torii-adapters/firebase';
-export default ToriiFirebaseAdapter.extend({
+import Object from '@ember/object';
+import { inject as service } from '@ember/service';
+
+export default Object.extend({
+    store: service(), // inject the ember-data store
+  
+    // The authorization argument passed in to `session.open` here is
+    // the result of the `torii.open(providerName)` promise
+    open: function(authorization){
+        const user = authorization.user;
+        return {
+            currentUser: user
+        };
+    }
 });
---
+++ app/torii-providers/firebase.js
@@ -0,0 +1,21 @@
+import Object from '@ember/object';
+import { Promise } from 'rsvp'
+import { inject as service } from '@ember/service';
+
+export default Object.extend({
+    firebaseApp: service(),    
+    open: function(options) {
+        const { email, password } = options;
+
+        return new Promise((resolve, reject) => {
+            this.get('firebaseApp').auth().then(auth => {
+                return auth.signInWithEmailAndPassword(email, password)
+            }).then((user) => {
+                resolve(user);
+            })
+            .catch((error) => {
+                reject(error)
+            });
+        });
+    }
+  });
