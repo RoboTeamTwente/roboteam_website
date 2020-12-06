136e5952b0a67c36d13218d9214ad6bdad2ce4f0
Lukas Bos <mrlukasbos@gmail.com>
2020-12-06 15:37:58 +0100

    start with design presentation index page

--- app/routes/design-presentation/index.js
+++ app/routes/design-presentation/index.js
@@ -1,4 +1,20 @@
 import Route from '@ember/routing/route';
+import { subteams } from 'roboteam-website/constants'
 
 export default Route.extend({
+  model() {    
+    return this.store.findAll('designitem').then(designitems => {
+      const sorted = designitems.sortBy('order');
+      const result = subteams.map(subteam => { 
+        const items =  sorted.filter(designitem => designitem.subteam === subteam.value);
+        if (items.length > 0) {
+          return {
+            subteam: subteam.title,
+            items
+          }
+        }
+      });
+      return result.filter(item => item);
+    });
+  }
 });
