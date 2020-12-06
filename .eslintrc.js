b0d6ca4b05a008a1058016d50761038eaf0bf859
Lukas Bos <mrlukasbos@gmail.com>
2020-12-06 09:47:32 +0100

    update lint`

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,14 @@
 module.exports = {
   root: true,
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: 2017,
     sourceType: 'module'
   },
-  plugins: [
-    'ember'
-  ],
-  extends: [
-    'eslint:recommended',
-    'plugin:ember/recommended'
-  ],
+  extends: 'eslint:recommended',
   env: {
     browser: true
   },
   rules: {
     "no-mixed-spaces-and-tabs": "off"
-  },
-  overrides: [
-    // node files
-    {
-      files: [
-        '.template-lintrc.js',
-        'ember-cli-build.js',
-        'testem.js',
-        'blueprints/*/index.js',
-        'config/**/*.js',
-        'lib/*/index.js'
-      ],
-      parserOptions: {
-        sourceType: 'script',
-        ecmaVersion: 2018
-      },
-      env: {
-        browser: false,
-        node: true
-      }
-    }
-  ]
+  }
 };
