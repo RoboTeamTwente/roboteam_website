d39350875f3cf7365e14ca6690a79c5a659c220d
sabinevanderwerff <hallo@sabinevanderwerff.nl>
2021-07-08 12:49:25 +0200

    Added sorting and a few improvements for the layout of the partner logos on the front page and corrected some minor spelling mistakes.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,7 +1,7 @@
 import Controller from '@ember/controller';
 import { computed } from '@ember/object';
 import { inject as service } from '@ember/service';
-import { filterBy } from '@ember/object/computed';
+//import { filterBy } from '@ember/object/computed';
 
 export default Controller.extend({
   moment: service(),
@@ -12,19 +12,48 @@ export default Controller.extend({
   selectedHeader: computed('model.headers', 'index', function() {
     return this.get('model.headers').objectAt(this.get('index'));
   }),
-
-   collaborators : filterBy('model.sponsors', 'package', "COLLABORATION"),
-
-   sponsors: computed('model.sponsors', function() {
-       return this.get('model.sponsors').filter(function(sponsor) {
-           return sponsor.get("package") !== "COLLABORATION";
-       });
-   }),
+ gigaByteSponsors: computed('model.sponsors', function() {
+		return this.get('model.sponsors').filter(function(sponsor) {
+			return sponsor.get('package') === "GIGABYTE";
+		});
+	}),
+	megaByteSponsors: computed('model.sponsors', function() {
+		return this.get('model.sponsors').filter(function(sponsor) {
+			return sponsor.get('package') === "MEGABYTE";
+		});
+	}),
+	kiloByteSponsors: computed('model.sponsors', function() {
+		return this.get('model.sponsors').filter(function(sponsor) {
+			return sponsor.get('package') === "KILOBYTE";
+		});
+	}),
+	byteSponsors: computed('model.sponsors', function() {
+		return this.get('model.sponsors').filter(function(sponsor) {
+			return sponsor.get('package') === "BYTE";
+		});
+	}),
+	demoByteSponsors: computed('model.sponsors', function() {
+		return this.get('model.sponsors').filter(function(sponsor) {
+			return sponsor.get('package') === "DEMOBYTE";
+		});
+	}),
+	collaborators: computed('model.sponsors', function() {
+		return this.get('model.sponsors').filter(function(sponsor) {
+			return sponsor.get('package') === "COLLABORATION";
+		});
+	}),
+//   collaborators : filterBy('model.sponsors', 'package', "COLLABORATION"),
+//
+//   sponsors: computed('model.sponsors', function() {
+//       return this.get('model.sponsors').filter(function(sponsor) {
+//           return sponsor.get("package") !== "COLLABORATION";
+//       });
+//   }),
 
   // Make a poll request to change the header automatically
   createPollingRequest() {
      let autoChangeHeader = () => {
-        this.set('index', (this.get('index') + 1) % this.get('model.headers.length')) 
+        this.set('index', (this.get('index') + 1) % this.get('model.headers.length'))
       }
       let pollingRequest = this.get('poll').addPoll({
         interval: 10 * 1000, //10 seconds
