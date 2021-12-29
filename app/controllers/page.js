378f07a4672677359c84d673abd5c7134ba3f936
fromHearts <52963150+fromHearts@users.noreply.github.com>
2021-12-29 13:02:31 +0100

    Unused imports

--- app/controllers/page.js
+++ app/controllers/page.js
@@ -1,6 +1,4 @@
 import Controller from '@ember/controller';
-import { computed } from '@ember/object';
-import { filter } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
 
 export default Controller.extend({
