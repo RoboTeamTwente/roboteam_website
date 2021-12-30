95408e3af4ec2340481d6eeeabb40724053b976d
Anne <52963150+fromHearts@users.noreply.github.com>
2021-12-30 13:43:31 +0100

    Make template consistent, add event history (#103)

--- app/controllers/events/index.js
+++ app/controllers/events/index.js
@@ -1,7 +1,17 @@
 import Controller from '@ember/controller';
+import { computed } from '@ember/object';
 import paginationMixin from 'roboteam-website/mixins/pagination-mixin'
+import pagedArray from 'ember-cli-pagination/computed/paged-array';
+import { alias } from '@ember/object/computed';
 
 export default Controller.extend(paginationMixin, {
   page: 1,
-  perPage: 6
+  perPage: 6,
+  orderedEvents: computed('model', function() {
+    return this.get('model').sortBy('startdate').toArray().reverse();
+  }),
+  pagedContent: pagedArray('orderedEvents', {
+    page: alias("parent.page"),
+    perPage: alias("parent.perPage")
+  }),
 });
