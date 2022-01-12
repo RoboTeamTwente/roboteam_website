a026144c8f351ea1ce8119a1034e4ae614ba8d99
fromHearts <52963150+fromHearts@users.noreply.github.com>
2022-01-12 09:45:29 +0100

    Fix issue with edit route returning 404

--- app/routes/base/edit.js
+++ app/routes/base/edit.js
@@ -1,5 +1,13 @@
-import ShowRoute from 'roboteam-website/routes/base/show';
+import Route from '@ember/routing/route';
 import RollbackModelOnDeactivationRouteMixin from 'roboteam-website/mixins/rollback-model-on-route-deactivation';
 import AuthenticatedRouteMixin from 'roboteam-website/mixins/authenticated-route';
 
-export default ShowRoute.extend(RollbackModelOnDeactivationRouteMixin, AuthenticatedRouteMixin, {});
+export const EditRoute = Route.extend({
+  modelName: null,
+  modelRouteParam: 'id',
+  model(params) {
+    return this.store.findRecord(this.get('modelName'), params[this.get('modelRouteParam')], params);
+  }
+});
+
+export default EditRoute.extend(RollbackModelOnDeactivationRouteMixin, AuthenticatedRouteMixin);
